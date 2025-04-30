e826f14ff8215221693af2852918be941ca91690
Phoebus <phoebuspetsi@gmail.com>
2025-04-30 13:36:10 +0300

    Added the Screens project.

---
+++ AndroidProjects/Screens/app/src/main/java/com/example/screens/FifteenthActivity.java
@@ -0,0 +1,14 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FifteenthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifteenth);
+    }
+}
