465f24c4f2e3c5a062620940bfbfefabd4617626
Phoebus <phoebuspetsi@gmail.com>
2025-05-03 12:48:08 +0300

    Remade the User Main Menu screen

--- AndroidProjects/Screens/app/src/main/java/com/example/screens/MainMenuActivity.java
+++ AndroidProjects/Screens/app/src/main/java/com/example/screens/MainMenuActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ThirdActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.main_menu_screen);
     }
 }
