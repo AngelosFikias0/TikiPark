66cfdba6f008753b67c141b8d4500faeea1888b0
Phoebus <phoebuspetsi@gmail.com>
2025-05-18 13:24:08 +0300

    More fixes. Stats and Wallet back buttons now work

--- AndroidProjects/TikiParkApp/app/src/main/java/com/example/tikiparkapp/activities/Stats.java
+++ AndroidProjects/TikiParkApp/app/src/main/java/com/example/tikiparkapp/activities/Stats.java
@@ -1,10 +1,15 @@
 package com.example.tikiparkapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tikiparkapp.R;
+
+import org.w3c.dom.Text;
 
 public class Stats extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class Stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_stats);
+
+        TextView amountSpent = findViewById(R.id.stats_amountSpent_txt);
+        TextView parkingTime = findViewById(R.id.stats_parkingTime_txt);
+        TextView totalReservations = findViewById(R.id.stats_reservations_txt);
+        Button backBtn = findViewById(R.id.stats_back_btn);
+
+        backBtn.setOnClickListener(v -> {
+            startActivity(new Intent(Stats.this, UserWelcome.class));
+            finish();
+        });
     }
 }
--- AndroidProjects/TikiParkApp/app/src/main/java/com/example/tikiparkapp/activities/WalletManagement.java
+++ AndroidProjects/TikiParkApp/app/src/main/java/com/example/tikiparkapp/activities/WalletManagement.java
@@ -1,6 +1,9 @@
 package com.example.tikiparkapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,24 @@ public class WalletManagement extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wallet);
+        setContentView(R.layout.act_wallet);
+
+        EditText amount = findViewById(R.id.wallet_amount_editTxt);
+        Button backBtn = findViewById(R.id.wallet_back_btn);
+        Button depositBtn = findViewById(R.id.wallet_deposit_btn);
+        Button withdrawBtn = findViewById(R.id.wallet_withdraw_btn);
+
+        backBtn.setOnClickListener(v -> {
+            startActivity(new Intent(WalletManagement.this, UserWelcome.class));
+            finish();
+        });
+
+        depositBtn.setOnClickListener(v -> {
+
+        });
+
+        withdrawBtn.setOnClickListener(v -> {
+
+        });
     }
 }
